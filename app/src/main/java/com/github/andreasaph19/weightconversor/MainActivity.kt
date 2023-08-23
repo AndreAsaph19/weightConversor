5775cf92f9c8ec3a2e6efdcae3d52ab6f7d335db
gabriel <gabrielpedradadev@gmail.com>
2023-08-23 14:49:21 -0300

    trabalho

--- app/src/main/java/com/github/andreasaph19/weightconversor/MainActivity.kt
+++ app/src/main/java/com/github/andreasaph19/weightconversor/MainActivity.kt
@@ -3,7 +3,7 @@ package com.github.andreasaph19.weightconversor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.andreasaph19.weightconversor.ActivityMainBinding
+import com.github.andreasaph19.weightconversor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,21 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calcular.setOnClickListener{ convertKilosToPounds() }
+        binding.calcular.setOnClickListener{ calcular() }
     }
 
-    fun convertKilosToPounds() {
-        val stringInTextField = binding.costOfService.text.toString()
+    fun calcular() {
+        val stringInTextField = binding.valor.text.toString()
         var valor = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val selectedId = binding.radio.checkedRadioButtonId
         if (selectedId == R.id.kg_lbs) {
             valor = valor * 2.2
             var resultadoString = String.format("Resultado:", valor)
-            binding.result.text = resultadoString
+            binding.resultado.text = resultadoString
         } else {
             valor = valor * 0.45
             var resultado = String.format("Resultado:", valor)
-            binding.result.text = resultado
+            binding.resultado.text = resultado
         }
 
     }
